90e45ce35e707cddb8aa0c9d50546c5fdbb58f73
Seven7Up <seven7up@seven7up.org>
2021-06-20 23:18:04 +0000

    BIG UPDATE

---
+++ Makefile
@@ -0,0 +1,24 @@
+.PHONY: usage, clean, sslsocket, socket
+
+# Change C compiler if you need
+CC=gcc
+CFLAGS=-g -Wall
+CERT=cert.pem
+
+all: usage
+
+usage:
+	@echo "Usage: make [sslsocket|socket]" >&2
+	@echo "" >&2
+	@echo "sslsocket: Basic ssl socket (https) in c from sslsocket.c file." >&2
+	@echo "socket: Basic socket (http) in c from socket.c file." >&2
+
+sslsocket: sslsocket.c
+	openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout $(CERT) -out $(CERT)
+	$(CC) $(CFLAGS) -o sslsocket sslsocket.c
+
+socket: socket.c
+	$(CC) $(CFLAGS) -o socket socket.c
+
+clean:
+	rm -f sslsocket socket
--- socket.c
+++ socket.c
@@ -12,10 +12,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <openssl/crypto.h>
-#include <openssl/ossl_typ.h>
-#include <openssl/ssl.h>
-
 void print_err(char msg[], ...){
   va_list comp;
   va_start(comp, msg);
@@ -36,25 +32,13 @@ int main(int argc, char *argv[]){
   char sendbuff[4096];
   int sendbytes;
   char recvbuff[4096];
-  SSL_CTX *ctx;
-  const SSL_METHOD *method;
-  SSL *ssl;
-
-  SSL_library_init();
-  SSL_load_error_strings();
-  OpenSSL_add_ssl_algorithms();
-
-  method = SSLv23_client_method();
-  ctx = SSL_CTX_new(method);
-  SSL_CTX_use_PrivateKey_file(ctx, "cert.pem", SSL_FILETYPE_PEM);
-  SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM);
 
   if ((net_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     print_err("Could not create a socket!\n");
 
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(443);
+  servaddr.sin_port = htons(80);
 
   host_entry = gethostbyname(argv[1]);
 
@@ -67,11 +51,6 @@ int main(int argc, char *argv[]){
   if ((connect(net_socket, (struct sockaddr *) &servaddr, sizeof(servaddr))) < 0)
     print_err("Could not connect to the address!\n");
 
-  ssl = SSL_new(ctx);
-  SSL_set_fd(ssl, net_socket);
-  SSL_connect(ssl);
-  SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
-
   bzero(&sendbuff, sizeof(sendbuff));
   sprintf(sendbuff,
           "GET %s HTTP/1.0\r\nHost: %s\r\nUser-Agent: "
@@ -80,17 +59,15 @@ int main(int argc, char *argv[]){
           argv[2], argv[1]);
 
   sendbytes = strlen(sendbuff);
-  if (SSL_write(ssl, sendbuff, sendbytes) != sendbytes)
+  if (write(net_socket, sendbuff, sendbytes) != sendbytes)
     print_err("Could not send the request!\n");
 
   bzero(&recvbuff, sizeof(recvbuff));
-  while (SSL_read(ssl, &recvbuff, 4095) > 0) {
+  while (read(net_socket, &recvbuff, 4095) > 0) {
     printf("%s\n", recvbuff);
     bzero(&recvbuff, sizeof(recvbuff));
   }
 
-  SSL_free(ssl);
-  SSL_CTX_free(ctx);
   close(net_socket);
 
   return 0;
